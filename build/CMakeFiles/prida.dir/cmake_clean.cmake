321c7d85abca648ad3e6895c159bde4b582cbe10
Bima Chandra <afusubakat@gmail.com>
2025-11-27 18:38:41 -0500

    init

---
+++ build/CMakeFiles/prida.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prida.dir/main.cpp.o"
+  "CMakeFiles/prida.dir/main.cpp.o.d"
+  "prida"
+  "prida.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prida.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
